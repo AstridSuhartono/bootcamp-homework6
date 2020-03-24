61f08d2a175eb72640063e6aa048e2251bf7390a
AstridSuhartono <astrid_sh@yahoo.com>
2020-03-25 01:25:01 +1030

    create some basic html and js but not finish yet

---
+++ script.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+    var infoTypeURL = "http://api.openweathermap.org/data/2.5/weather?+q=";
+    var APIkey = "4ee5795f0338dedf641f1c65d49e8b9c";
+    var queryURL = infoTypeURL+"&units=metric&appid="+APIkey;
+
+    // Make the AJAX request to the API - GETs the JSON data at the queryURL.
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+        }).then(function(response){
+        console.log(queryURL);
+        console.log(response);
+        });
+
+});
